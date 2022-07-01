97c80f9693c6770df55929bfb57eecb70cca036c
RebeccaFolarin <beccafolarin12@gmail.com>
2022-06-30 19:51:58 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
-
-/**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
-int main(void)
-{
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
-}
+/
+ * main - Entry point
+ *  * Return: Always 0 (sucess)
+ *   */
+ int main(void)																					{																						puts("\"Programming is like building a multilingual puzzle");
+return (0);																					}			 
