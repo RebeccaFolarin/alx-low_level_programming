951de046cabaed6a9e8476160fa8eec2c05810ed
RebeccaFolarin <beccafolarin12@gmail.com>
2022-07-07 09:00:09 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
-* main - prints putchar
-* Return: always 0
-*/
+ * main - prints "putchar"
+ * Return: 0
+ */
 int main(void)
 {
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char putchar[] = "putchar";
+	int i = 0;
+
+	while (putchar [i] != '\0')
+	{
+		_putchar(putchar[i]);
+		i++;
+	}
 	_putchar('\n');
 
 	return (0);
-
 }
 
