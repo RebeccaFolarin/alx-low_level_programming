0e615370e9137b1c502f73b51e95e9caf8cf50fb
RebeccaFolarin <beccafolarin12@gmail.com>
2022-08-26 12:53:49 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints only even numbered indexes
+ * @str: Checks for str
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
+
