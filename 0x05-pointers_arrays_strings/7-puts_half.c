606b4338bb3fea8cc9c5c225c20a914694792300
RebeccaFolarin <beccafolarin12@gmail.com>
2022-08-31 13:42:25 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints second half of string
+ * @str: Checks for char str
+ */
+
+void puts_half(char *str)
+{
+	int len = 0;
+	int i = 0;
+	int j;
+
+	for (len = 0; str[len] != '\0'; len++)
+
+	if ((len % 2) == 0)
+		j = len / 2;
+
+	else
+		j = (len + 1) / 2;
+
+	for (i = j; i < len; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
+
